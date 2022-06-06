e3bea1cec8d1d7c1ef70a6617d5da90a28d1d460
Taha-Adeel <cs20btech11052@iith.ac.in>
2022-06-07 02:47:31 +0530

    Prototype 1

---
+++ Prototype 1(Car game+Plane game)/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    private float color_a = 0.4f;
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.5f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.5f, 1.0f, 0.3f, color_a);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(15.0f * Time.deltaTime, 0.0f, 0.0f);
+        color_a = (color_a + 0.5f * Time.deltaTime)%1.0f;
+        Material material = Renderer.material;
+        material.color = new Color(0.5f, 1.0f, 0.3f, color_a);
+    }
+}
