7906f1dba59944975f82ad8959ad7c2fd2fe7704
Taha-Adeel <cs20btech11052@iith.ac.in>
2022-06-08 03:57:56 +0530

    Implemented proper scene flow, and data persistence across multiple scenes and sessions

--- Jr Programming Project/Assets/Scripts/UI/UIMainScene.cs
+++ Jr Programming Project/Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
--- Jr Programming Project/Assets/Scripts/Unit.cs
+++ Jr Programming Project/Assets/Scripts/Unit.cs
@@ -26,7 +26,8 @@ public abstract class Unit : MonoBehaviour,
 
     private void Start()
     {
-
+        if(MainManager.Instance != null)
+            SetColor(MainManager.Instance.TeamColor);
     }
 
     void SetColor(Color c)
